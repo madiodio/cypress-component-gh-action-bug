c1a5dfa207a8d6c30adfbc23b7f0e00a71fe5573
Madiodio Gaye <papigaye1@gmail.com>
2021-07-16 11:38:01 +0000

    ci: :green_heart: Add support & fixtures folders

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,6 @@
 module.exports = (on, config) => {
+  require("@cypress/code-coverage/task")(on, config);
+
   if (config.testingType === 'component') {
     require('@cypress/react/plugins/react-scripts')(on, config)
   }
